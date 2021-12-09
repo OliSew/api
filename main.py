f6440026818b0cbc8797c61a84cb65c6395398e0
OliSew <oliviersewera@wp.pl>
2021-12-09 13:32:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#This is a simple computer program with which you can chat with the api
+print("api: What is your name?")
+name = input("My name is: ")
+print("Where are you from?")
+country = input("I am from: ")
+print("api: What is your hobby?")
+hobby = input("My hobby is: ")
+name1 = f"Heloo {name}"
+country1 = f"From {country}"
+hobby1 = f"That enjoys {hobby}"
+print(name1)
+print(country1)
+print(hobby1)
